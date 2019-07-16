ec1ed00c7a7907681e5f48f6286a9d572354c965
jason.yang <jason.yang@servicenow.com>
2019-07-15 23:29:37 -0700

    update

--- src/main/java/my/App.java
+++ src/main/java/my/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "in branch 1 - 2: Hello World!" );
+        System.out.println( "in branch 1 - 3: Hello World!" );
     }
 }
